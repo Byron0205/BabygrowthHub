f1aedc0c04d141d9a830255674769bf9615025cd
Byron0205 <gabossosa@gmail.com>
2023-07-03 15:45:37 -0600

    feat(router): agregado de rutas en error,usuarios, auditoria

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path:'/:pathMatch(.*)',
+      name: '404',
+      component: ()=> import ("../views/NotFound.vue")
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
@@ -16,6 +21,21 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: "/admin",
+      name: 'admin',
+      component: ()=> import('../views/AdminView.vue')
+    },
+    {
+      path: "/admin/usuarios",
+      name: "usuarios",
+      component: ()=> import('../views/UsuariosView.vue')
+    },
+    {
+      path: "/admin/auditoria",
+      name: "auditoria",
+      component: ()=> import("../views/AuditoriaView.vue")
     }
   ]
 })
---
+++ src/views/AdminView.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Vista de administrador</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
