7cdbf5a84c8292b732dad480fe1d6a50bfd93dcf
Byron0205 <gabossosa@gmail.com>
2023-07-03 18:05:35 -0600

    feat(navbar): agregado del navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav>
+        <div class="container-img">
+            <img class="img" src="../assets/img/Fondo-de-nubes.png" alt="fondo nubes">
+
+        </div>
+        <div class="container-logo">
+            <img class="img-logo" src="../assets/img/logo.png" alt="logo">
+        </div>
+        <div class="flex">
+            <ul class="navbar">
+                <li class="item">Inicio</li>
+                <li class="item">Expendiente&Medicacion</li>
+                <li class="item">Dietas</li>
+                <li class="item">Preguntas&Respuestas</li>
+                <li class="item">Actividad</li>
+                <li class="item">Perfil</li>
+            </ul>
+            <!-- <div>
+                <button class="btn">Salir</button>
+            </div> -->
+            <div class="btn-group">
+                <button class="btn-inicio">Iniciar Sesion</button>
+                <button class="btn-registro">Registrarse</button>
+            </div>
+            
+        </div>
+    </nav>
+    <div class="container-img">
+        <img src="../assets/img/Efecto-nube.png" alt="efecto">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
