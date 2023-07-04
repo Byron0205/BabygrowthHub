9e507b00565bfbab822e7908647c12456e33a3ae
Brian <briangrji28@gmail.com>
2023-07-04 17:53:23 -0600

    Front nombres y significados

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
-<script setup></script>
-
 <template>
   <div class="home-flex-container">
     <div class="home-text">
       <div class="home-decoracion">
-        <img class="home-nave-espacial" src="../assets/img/Decoracion-nave-espacial.png" alt="Decoracion nave espacial">
+        <img
+          class="home-nave-espacial"
+          src="../assets/img/Decoracion-nave-espacial.png"
+          alt="Decoracion nave espacial"
+        />
       </div>
       <p class="home-logo">
         ¡Bienvenido a <span class="home-blue-text">BabyGrowthHub</span>!
@@ -34,4 +36,47 @@
       />
     </div>
   </div>
+
+  <!-- 'NS' = Nombres y significados -->
+  <div class="NS-flex-container">
+    <div class="NS-img-container">
+      <img
+        class="NS-img"
+        src="../assets/img/Home-image-02.png"
+        alt="Niños jugando"
+      />
+    </div>
+    <div class="NS-text">
+      <p class="NS-titulo">Nombres & Significados</p>
+      <p class="NS-parrafo">
+        ¡Descubre el significado detrás de cada nombre de bebé con nuestro
+        buscador especializado! Simplemente ingresa el nombre que deseas
+        explorar y haz clic en el botón de búsqueda
+      </p>
+      <div class="NS-buscar-container">
+        <input type="text" class="NS-input" />
+        <button class="NS-button">
+          <i class="fa-solid fa-magnifying-glass NS-icon"></i>
+        </button>
+      </div>
+
+      <!-- Ocultar y mostrar segun sea necesario -->
+      <div id="ResultadoSignificado" class="NS-resultado-container">
+        <p class="NS-resultado" id="Resultado">
+          {{ resultadoSignificado }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        resultadoSignificado: "El nombre Brian tiene origen celta y su significado es 'fuerte' o 'poderoso'. Proviene de la palabra irlandesa 'Brígh' que significa 'fuerza' o 'virtud'."
+      }
+    },
+  }
+</script>
