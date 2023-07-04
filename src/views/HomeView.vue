de46a2b68304bd95555a47208880aa7feb789726
Brian <briangrji28@gmail.com>
2023-07-04 16:17:43 -0600

    Crear página inicio antes de iniciar sesión

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,37 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <h1>home view</h1>
-  </main>
+  <div class="home-flex-container">
+    <div class="home-text">
+      <div class="home-decoracion">
+        <img class="home-nave-espacial" src="../assets/img/Decoracion-nave-espacial.png" alt="Decoracion nave espacial">
+      </div>
+      <p class="home-logo">
+        ¡Bienvenido a <span class="home-blue-text">BabyGrowthHub</span>!
+      </p>
+      <p class="home-parrafo">
+        Te ofrecemos una amplia gama de servicios diseñados para ayudarte a
+        cuidar y mantener a tu pequeño tesoro feliz y saludable. Nos enorgullece
+        brindarte una experiencia sencilla, intuitiva y agradable mientras te
+        acompañamos en esta hermosa etapa de la vida. ¡Únete a nosotros y
+        descubre cómo hacer el cuidado de tu bebé más fácil y emocionante que
+        nunca!A contuación enlaces de interes:
+      </p>
+
+      <!-- Queda pendiente modificar los enlaces -->
+      <div class="home-container-enlaces">
+        <a class="home-enlace" href="#">Consejos</a>
+        <a class="home-enlace" href="#">Nombres & Significados</a>
+        <a class="home-enlace" href="#">Acerca De</a>
+      </div>
+    </div>
+
+    <div class="home-img-container">
+      <img
+        class="home-img"
+        src="../assets/img/Home-image-01.png"
+        alt="Niños jugando"
+      />
+    </div>
+  </div>
 </template>
