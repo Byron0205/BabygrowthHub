22bfcbe3572cef6aee6f9f9ede35ddab0d7b8534
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-07-16 16:51:38 -0600

    formulario con items acomodados

--- src/views/Multimedia/SubirMultimediaView.vue
+++ src/views/Multimedia/SubirMultimediaView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div class="img-container">
+    <div class="container">
+        <div class="info-container">
             <img src="../../assets/img/Imagen-decoracion.png" alt="">
             <p class="info-text">Sumérgete en un mundo encantador donde las imágenes y
                 videos se convierten en tesoros de los momentos más
@@ -8,33 +8,41 @@
                 Comparte en este apartado tus <strong>imagenes y videos</strong> más presiados de tus tesoros!</p>
         </div>
         <div class="form-multi">
+            <div class="memory">
+                <h2>Nuevo recuerdo</h2>
+            </div>
             <label for="new-memory" class="text-label">Nombre Recuerdo</label>
             <input type="text" class="new-memory">
             <div class="upload-multi" @dragover="onDragOver" @dragleave="onDragLeave" @drop="onDrop"
                 :class="{ 'drag-over': isDragging }">
                 <p class="date">{{ formattedDate }}</p>
-                
                 <!-- Mostrar el nombre del archivo subido -->
                 <p v-if="uploadedFile" class="drag-text">{{ uploadedFile.name }}</p>
                 <p v-else class="drag-text">Arrastre y suelte la imagen / video a subir aquí</p>
-                
                 <!-- Mostrar el icono del basurero y permitir eliminar el archivo -->
-                <i v-if="uploadedFile" @click="deleteUploadedFile" class="trash-icon fas fa-trash" style="color: #4F439A;"></i>
+                <i v-if="uploadedFile" @click="deleteUploadedFile" class="trash-icon fas fa-trash"
+                    style="color: #4F439A;"></i>
             </div>
-            <label for="type-multi" class="text-label">Seleccione el álmum destino</label>
-            <select class="type-multi">
-                <option value="etapas">Etapas de desarrollo</option>
-                <option value="ultrasonidos">Ultrasonidos</option>
-            </select>
-            <label for="age-baby" class="text-label">Seleccione el álmum destino</label>
-            <select class="age-baby">
-                <option value="0">0 años</option>
-                <option value="1">1 año</option>
-                <option value="2">2 años</option>
-                <option value="3">3 años</option>
-                <option value="4">4 años</option>
-                <option value="5">5 años</option>
-            </select>
+            <div class="select-container">
+                <div class="select-wrapper">
+                    <label for="type-multi" class="text-label">Seleccione el álbum destino</label>
+                    <select class="type-multi">
+                        <option value="etapas">Etapas de desarrollo</option>
+                        <option value="ultrasonidos">Ultrasonidos</option>
+                    </select>
+                </div>
+                <div class="select-wrapper">
+                    <label for="age-baby" class="text-label">Seleccione la etapa para situar el archivo</label>
+                    <select class="age-baby">
+                        <option value="0">0 años</option>
+                        <option value="1">1 año</option>
+                        <option value="2">2 años</option>
+                        <option value="3">3 años</option>
+                        <option value="4">4 años</option>
+                        <option value="5">5 años</option>
+                    </select>
+                </div>
+            </div>
             <label for="sons" class="text-label">Asociar con tu pequeño/a</label>
             <select class="sons">
                 <option value="1">Ismael Rosales Mora</option>
@@ -81,7 +89,7 @@ export default {
         onDrop(event) {
             event.preventDefault();
             this.isDragging = false;
-            
+
             // Manejar el archivo(s) subido aquí
             const files = event.dataTransfer.files;
             console.log(files);
