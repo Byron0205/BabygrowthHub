75b13138f6539c6fb63cac2d46482ac394255645
Brian <briangrji28@gmail.com>
2023-07-16 15:10:08 -0600

    Select baby and time of event in calendar

--- src/components/Calendario.vue
+++ src/components/Calendario.vue
@@ -25,7 +25,10 @@
           <td class="cellsCalendar" v-for="day in week" :key="day.date">
             <!-- Metodos 'isToday' y 'isSelected retornan boolean y segun este se aplica la clase 'today' o 'isSelected'' -->
             <div
-              :class="{ calendarToday: isToday(day), isSelected: isSelected(day) }"
+              :class="{
+                calendarToday: isToday(day),
+                isSelected: isSelected(day),
+              }"
               @click="selectDate(day)"
             >
               <span>{{ day.day }}</span>
@@ -51,7 +54,9 @@
 
         <div class="formCreateNewEvent">
           <div class="newEventFlexContainer">
-            <label class="newEventLabel" for="newEventName">Nombre Actividad</label>
+            <label class="newEventLabel" for="newEventName"
+              >Nombre Actividad</label
+            >
             <input
               class="newEventInput"
               type="text"
@@ -60,27 +65,92 @@
             />
           </div>
           <div class="newEventFlexContainer">
-            <label class="newEventLabel" for="newEventDescription">Detalle:</label>
-            <textarea class="newEventInput"
+            <label class="newEventLabel" for="newEventDescription"
+              >Detalle</label
+            >
+            <textarea
+              class="newEventInput newEventTextArea"
               id="newEventDescription"
               v-model="eventDescription"
             ></textarea>
           </div>
           <div class="newEventFlexContainer">
-            <label for="newEventDate">Fecha:</label>
-            <input type="text" id="newEventDate" v-model="eventDate" readonly />
+            <label class="newEventLabel" for="newEventDate">Fecha</label>
+            <input
+              class="newEventInput"
+              type="text"
+              id="newEventDate"
+              v-model="eventDate"
+              readonly
+            />
           </div>
-          <div>
-            <label for="newEventCategory">Categoría:</label>
-            <select id="newEventCategory" v-model="eventCategory">
-              <option
-                v-for="category in categories"
-                :key="category.name"
-                :value="category"
+
+          <div class="newEventFlexContainer row">
+            <div class="newEventFlexContainer">
+              <label class="newEventLabel" for="newEventPriority"
+                >Seleccione una prioridad</label
               >
-                {{ category.name }}
+              <select
+                class="newEventSelect"
+                id="newEventPriority"
+                v-model="eventPriority"
+              >
+                <option
+                  v-for="priority in priorities"
+                  :key="priority.id"
+                  :value="priority.name"
+                >
+                  {{ priority.name }}
+                </option>
+              </select>
+            </div>
+            <div class="newEventFlexContainer">
+              <label class="newEventLabel" for="newEventCategory"
+                >Seleccione una categoria</label
+              >
+              <select
+                class="newEventSelect"
+                id="newEventCategory"
+                v-model="eventCategory"
+              >
+                <option
+                  v-for="category in categoriesList"
+                  :key="category.id"
+                  :value="category"
+                >
+                  {{ category.name }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <div class="newEventFlexContainer">
+            <label class="newEventLabel" for="newEventBaby"
+              >Actividad asociada a</label
+            >
+            <select
+              class="newEventSelect babieSelect"
+              id="newEventBaby"
+              v-model="eventBaby"
+            >
+              <option
+                v-for="babie in babiesList"
+                :key="babie.id"
+                :value="babie.name"
+              >
+                {{ babie.name }}
               </option>
             </select>
+          </div>
+          <div class="newEventFlexContainer">
+            <label class="newEventLabel" for="eventTime"
+              >Seleccione una hora</label
+            >
+            <input
+              class="newEventSelect"
+              type="time"
+              id="eventTime"
+              v-model="eventTime"
+            />
           </div>
 
           <div>
@@ -113,14 +183,19 @@ export default {
       daysOfWeek: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
       eventName: "",
       eventDescription: "",
+      eventTime: "",
       isPopupOpen: false,
       eventDate: null,
-      categories: [
-        { name: "Cita Médica", color: "#87bd40" },
-        { name: "Tratamiento", color: "#4081bd" },
-        { name: "Personal", color: "#7e40bd" },
+      categories: [],
+      priorities: [
+        { id: "1", name: "Alta" },
+        { id: "2", name: "Media" },
+        { id: "3", name: "Baja" },
       ],
       eventCategory: null,
+      eventPriority: null,
+      eventBaby: null,
+      registeredBabies: [],
     };
   },
   computed: {
@@ -154,6 +229,12 @@ export default {
 
       return days;
     },
+    babiesList() {
+      return this.registeredBabies;
+    },
+    categoriesList() {
+      return this.categories;
+    },
   },
   methods: {
     previousMonth() {
@@ -183,19 +264,28 @@ export default {
     },
     saveEvent() {
       const selectedCategory = this.eventCategory;
+      const selectedPriority = this.eventPriority;
+      const selectedBaby = this.eventBaby;
+
       const event = {
         date: format(this.selectedDate, "yyyy-MM-dd"),
         title: this.eventName,
         description: this.eventDescription,
         category: selectedCategory.name,
+        priority: selectedPriority.name,
+        babie: selectedBaby.name,
         color: selectedCategory.color,
+        time: this.eventTime,
       };
       this.events.push(event);
 
       // Reset input values
       this.eventName = "";
       this.eventDescription = "";
+      this.eventPriority = null;
       this.eventCategory = null;
+      this.eventBaby = null;
+      this.eventTime = "";
 
       this.isPopupOpen = false;
     },
@@ -214,6 +304,20 @@ export default {
       );
       return category ? category.color : "";
     },
+  },
+  mounted() {
+    // Obtener los datos de la BD para llenar el select de los bebes
+    this.registeredBabies = [
+      { id: 1, name: "Brian G" },
+      { id: 2, name: "Juanito D" },
+      { id: 3, name: "Veronica S" },
+    ];
+
+    this.categories = [
+      { name: "Cita Médica", color: "#87bd40" },
+      { name: "Tratamiento", color: "#4081bd" },
+      { name: "Personal", color: "#7e40bd" },
+    ];
   },
 };
 </script>
