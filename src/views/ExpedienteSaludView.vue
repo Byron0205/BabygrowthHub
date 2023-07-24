ad4492ab814d2fc3eef1501619d11ab3317553b7
Byron0205 <gabossosa@gmail.com>
2023-07-24 13:16:28 -0600

    cambios ajustes imagenes

--- src/views/ExpedienteSaludView.vue
+++ src/views/ExpedienteSaludView.vue
@@ -6,7 +6,7 @@
                 <router-link class="link-menu" :to="'/expediente/salud/'+ this.idBebe">Salud</router-link>
             </div>
             <div class="item-lateral">
-                <img src=".https://baby-growth-hub.s3.amazonaws.com/ImagenesSitioWeb/img/icono-beaker.svg" alt="reloj">
+                <img src="https://baby-growth-hub.s3.amazonaws.com/ImagenesSitioWeb/img/icono-beaker.svg" alt="reloj">
                 <router-link class="link-menu" :to="'/expediente/medicacion/'+ this.idBebe">Medicación</router-link>
             </div>
         </MenuLateral>
