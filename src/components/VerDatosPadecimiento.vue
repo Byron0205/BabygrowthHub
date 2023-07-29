f990a14fe6559fb651403f93c746b64731459f77
Brian <briangrji28@gmail.com>
2023-07-29 16:27:11 -0600

    Eliminar alergias y diagnosticos

--- src/components/VerDatosPadecimiento.vue
+++ src/components/VerDatosPadecimiento.vue
@@ -1,131 +1,149 @@
 <template>
-    <div class="viewMedicalData">
-        <!-- <p class="description-MedicalData">
-            Selecciona alguna de las siguientes opciones para poder ver los datos
-        </p> -->
-        <div class="select-Data">
-            <!-- <input type="radio" id="diagnosticos" name="opciones" value="opcion1"> -->
-            <!-- <label for="diagnosticos" class="radio-option">Diagnósticos médicos</label> -->
-
-            <!-- <input type="radio" id="padecimientos" name="opciones" value="opcion3"> -->
-            <label for="padecimientos" class="radio-option">Padecimientos registrados</label>
-        </div>
-        <div class="data-view-list">
-            <div class="view-item" v-for="item in Padecimientos" :key="item.id">
-                <div class="text-item">
-                    <img src="https://baby-growth-hub.s3.amazonaws.com/ImagenesSitioWeb/img/icono-texto.svg" alt="icon">
-                    <p class="text-item-style">{{ item.Padecimiento }}</p>
-                </div>
-                <!-- <img class="icon-trash" @click="deleteDiagnostico(item.id)" src="../assets/img/icono-trash.svg"
-                    alt="icon trash"> -->
-            </div>
-        </div>
+  <div class="viewMedicalData">
+    <p class="description-MedicalData">
+      Selecciona alguna de las siguientes opciones para poder ver los datos
+    </p>
+    <div class="select-Data">
+      <button
+        class="button-expediente diagnositoco"
+        @click="obtenerDiagnosticos(this.idBebe)"
+      >
+        Diagnósticos médicos
+      </button>
+      <button
+        class="button-expediente alergias"
+        @click="obtenerAlergias(this.idBebe)"
+      >
+        Alergias registrados
+      </button>
     </div>
+    <div class="data-view-list">
+      <div class="view-item" v-for="item in Padecimientos" :key="item.id">
+        <div class="text-item">
+          <i class="fa-solid fa-align-left icon-text"></i>
+          <p class="text-item-style">{{ item.Padecimiento }}</p>
+        </div>
+        <i
+          class="fa-solid fa-trash icon-trash"
+          @click="deleteDiagnostico(item.id, item.type)"
+        ></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import { id } from "date-fns/locale";
 export default {
-    data() {
-        return {
-            diagnosticos: [
-                { diagnostico: 'Dermatitis del pañal', id: 1 },
-                { diagnostico: 'Cólicos del lactante', id: 2 },
-                { diagnostico: "Infecciones del tracto respiratorio superior", id: 3 },
-                { diagnostico: 'Dermatitis del pañal', id: 4 },
-                { diagnostico: 'Cólicos del lactante', id: 5 },
-                { diagnostico: "Infecciones del tracto respiratorio superior", id: 6 },
-                { diagnostico: 'Dermatitis del pañal', id: 7 },
-                { diagnostico: 'Cólicos del lactante', id: 8 },
-                { diagnostico: "Infecciones del tracto respiratorio superior", id: 9 }
-            ],
-            Padecimientos:[],
-            PDiagnosticos:[],
-            PAlergias:[]
+  data() {
+    return {
+      Padecimientos: [],
+      Diagnosticos: [],
+      Alergias: [],
+      idbebe: "",
+      message: "",
+      alertType: "",
+    };
+  },
+  methods: {
+    deleteDiagnostico(id, type) {
+      if (type == "diagnostico") {
+        const url = "http://localhost:3000/eliminar-diagnostico/" + id;
+        axios
+          .get(url)
+          .then((response) => {
+            console.log(response.data);
+            if (response.data) {
+              this.message = "¡Diagnostico eliminado exitosamente!";
+              this.alertType = "success";
+              this.showAlert();
+              this.$router.push(`/expediente/salud/${this.idbebe}`);
+            } else {
+              this.message = "¡Ha ocurrido al eliminar la diagnostico!";
+              this.alertType = "error";
+              this.showAlert();
+            }
+          })
+          .catch((err) => {
+            console.error("Error al obtener los datos: " + err);
+          });
+      } else {
+        const url = "http://localhost:3000/eliminar-alergia/" + id;
+        axios
+          .get(url)
+          .then((response) => {
+            console.log(response.data);
+            if (response.data) {
+              this.message = "¡Alergia eliminada exitosamente!";
+              this.alertType = "success";
+              this.showAlert();
+              this.$router.push(`/expediente/salud/${this.idbebe}`);
+            } else {
+              this.message = "¡Ha ocurrido al eliminar la alergia!";
+              this.alertType = "error";
+              this.showAlert();
+            }
+          })
+          .catch((err) => {
+            console.error("Error al obtener los datos: " + err);
+          });
+      }
+    },
+    obtenerDiagnosticos(id) {
+      console.log(id);
+      this.Diagnosticos = [];
+      this.Padecimientos = [];
+      const url = "http://localhost:3000/verDiagnosticos/" + id;
+      axios
+        .get(url)
+        .then((response) => {
+          this.Diagnosticos = response.data;
+          this.Padecimientos = this.Diagnosticos.map((diagnostico) => ({
+            ...diagnostico,
+            type: "diagnostico",
+          }));
+        })
+        .catch((err) => {
+          console.error("Error al obtener los datos: " + err);
+        });
+    },
+    obtenerAlergias(id) {
+      this.Alergias = [];
+      this.Padecimientos = [];
+      const url = "http://localhost:3000/verAlergias/" + id;
+      axios
+        .get(url)
+        .then((response) => {
+          this.Alergias = response.data;
+          this.Padecimientos = this.Alergias.map((alergia) => ({
+            ...alergia,
+            type: "alergia",
+          }));
+        })
+        .catch((err) => {
+          console.error("Error al obtener los datos: " + err);
+        });
+    },
+    showAlert() {
+      const displayDuration = 3000;
 
-        }
+      setTimeout(() => {
+        this.message = "";
+        this.alertType = "";
+      }, displayDuration);
     },
-    methods: {
-        deleteDiagnostico(id) {
-            this.diagnosticos = this.diagnosticos.filter(elemento => elemento.id !== id);
-        },
-        obtenerDiagnosticos(id) {
-            const url = 'https://tiusr3pl.cuc-carrera-ti.ac.cr/verDiagnosticos/' + id
-            axios.get(url)
-                .then(response => {
-                    this.PDiagnosticos = response.data;
-                    this.Padecimientos.push(...this.PDiagnosticos)
-                })
-                .catch(err => {
-                    console.error('Error al obtener los datos: ' + err)
-                })
-        },
-        obtenerAlergias(id) {
-            const url = 'https://tiusr3pl.cuc-carrera-ti.ac.cr/verAlergias/' + id
-            axios.get(url)
-                .then(response => {
-                    this.PAlergias= response.data;
-                    this.Padecimientos.push(...this.PAlergias)
-                })
-                .catch(err => {
-                    console.error('Error al obtener los datos: ' + err)
-                })
-        },
-    },
-    mounted(){
-        const idBebe = this.$route.params.id;
-        this.obtenerDiagnosticos(idBebe)
-        this.obtenerAlergias(idBebe)
-    }
-}
+  },
+  mounted() {
+    console.log(this.$route.params.id);
+    this.idBebe = this.$route.params.id;
+  },
+};
 </script>
 
 <style scoped>
-/* Estilo para ocultar los radios por defecto */
-input[type="radio"] {
-    position: absolute;
-    opacity: 0;
-    width: 0;
-    height: 0;
-}
-
-/* Estilo para las etiquetas de los radios */
-label {
-    display: inline-block;
-    padding: 10px 20px;
-    font-family: Arial, sans-serif;
-    font-size: 16px;
-}
-
-.radio-option {
-    width: 100%;
-    text-align: center;
-    font-family: 'Baloo 2';
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-    border-radius: 15px;
-}
-
-.select-Data{
-    margin-top: 20px;
-}
-
-.select-Data>.radio-option:nth-of-type(1) {
-    background-color: #DAEFFE;
-    color: #479DD8;
-}
-
-
-.select-Data>.radio-option:nth-of-type(2) {
-    background-color: #F4D2DE;
-    color: #E4547D;
-}
-
-
-/* Estilo para la etiqueta cuando el radio está seleccionado */
-input[type="radio"]:checked+label {
-    border: 2px dashed black;
+.icon-text {
+  color: #0f5b8e;
+  margin-top: 1.5rem;
 }
 </style>
